49d7ddc224e8dcd95f6ca83a04c90371dfe85ee3
Chris Thompson <chris.thompson@brunyview.com>
2022-06-09 12:28:30 +1000

    feat: bootstap app component

--- src/app/tab2/tab2.routing.ts
+++ src/app/tab2/tab2.routing.ts
@@ -4,6 +4,6 @@ import { Tab2Page } from './tab2.page';
 export const routes: Routes = [
   {
     path: '',
-    component: Tab2Page,
+    loadComponent: () => import('./tab2.page').then((m) => m.Tab2Page),
   },
 ];
--- src/app/tab3/tab3.routing.ts
+++ src/app/tab3/tab3.routing.ts
@@ -4,6 +4,6 @@ import { Tab3Page } from './tab3.page';
 export const routes: Routes = [
   {
     path: '',
-    component: Tab3Page,
+    loadComponent: () => import('./tab3.page').then((m) => m.Tab3Page),
   },
 ];
